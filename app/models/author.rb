574e12dd8aeb98f3ab9d14977d606f15c52002ed
Dex Fitch <fitch.dexter@gmail.com>
2021-11-04 14:09:38 -0400

    Done~

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,26 @@
 class Author < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :phone_number, length: { is: 10 }
+    validate :proper_name_case
+    validate :not_a_hollywood_number
+
+    private
+
+    def proper_name_case
+        if self.name != self.name.titleize
+            errors.add(:name, "Must be properly name cased")
+        end
+    end
+    
+    def not_a_hollywood_number
+        string_phone = self.phone_number.to_s
+        call_code = string_phone[3..5]
+
+        if string_phone.start_with?("555")
+            errors.add(:phone_number, "Must not start with 555")
+        elsif call_code == "555"
+            errors.add(:phone_number, "Calling code must not be 555")
+        end
+    end
+    
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, exclusion: { in: ["True Facts"] }
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+    validate :goldilocks_clickbait
+
+    def goldilocks_clickbait
+        clickbait_titles = ["Won't Believe", "Secret", "Top", "Guess"]
+
+        valid_clickbaits = []
+
+        clickbait_titles.each do |clickbait|
+            if self.title.include?(clickbait)
+                valid_clickbaits << clickbait
+            end
+        end
+
+        if valid_clickbaits.length == 0
+            errors.add(:title, "Not clickbait enough")
+        end
+    end
+
 end
